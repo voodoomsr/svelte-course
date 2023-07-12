bbe1c3188f468287432b5e8fdfb7055fbe40b541
voodoomsr <mjsr@outlook.com>
2023-07-11 21:37:56 -0400

    7 Svelte Components Basic Structure

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = 'Svelte';
+  let src = 'http://picsum.com/200/200';
+  let string = 'some <strong>bold</strong> text'
+</script>
+
+
+<img {src} alt='Some text {name}' />
+<h1>Hello {name.toUpperCase()}</h1>
+<p>{@html string}</p>
+<style>
+  h1 {
+    color: red;
+  }
+</style>
